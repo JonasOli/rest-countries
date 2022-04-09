dd7c04f5a832bfe338099cebff6e6bfbb777f39e
Jonas Oliveira <jonasfissicaro13@gmail.com>
2022-04-09 18:17:10 -0300

    search country input

--- src/pages/ListCountries/index.tsx
+++ src/pages/ListCountries/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import CountryCard from "components/CountryCard";
+import React, { useState } from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useQuery } from "react-query";
-import CountryCard from "components/CountryCard";
 import { listAllCountries } from "services/contriesService";
 import { ListCoutriesContainer } from "./ListContries.styles";
 
 const ListCountries = () => {
+  const [search, setSearch] = useState<RegExp>(new RegExp(""));
   const { isLoading, data } = useQuery("listAllCountries", listAllCountries, {
     refetchOnWindowFocus: false,
   });
@@ -13,11 +14,19 @@ const ListCountries = () => {
   if (isLoading) return <AiOutlineLoading3Quarters />;
 
   return (
-    <ListCoutriesContainer>
-      {data?.map((country) => (
-        <CountryCard key={country.name} {...country} />
-      ))}
-    </ListCoutriesContainer>
+    <>
+      <input
+        type="text"
+        onChange={(evt) => setSearch(new RegExp(evt.currentTarget.value, "gi"))}
+      />
+      <ListCoutriesContainer>
+        {data
+          ?.filter((country) => country.name.match(search))
+          .map((country) => (
+            <CountryCard key={country.name} {...country} />
+          ))}
+      </ListCoutriesContainer>
+    </>
   );
 };
 
